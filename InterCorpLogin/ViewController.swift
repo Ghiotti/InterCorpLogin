2ac1c3b6d8310b2ba9dc539239ea1cb8391750b9
Franco Ghiotti <fran.ghiotti@gmail.com>
2019-07-05 11:29:51 -0300

    start new proyect

---
+++ InterCorpLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterCorpLogin
+//
+//  Created by Franco Ghiotti on 05/07/2019.
+//  Copyright © 2019 Franco Ghiotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
